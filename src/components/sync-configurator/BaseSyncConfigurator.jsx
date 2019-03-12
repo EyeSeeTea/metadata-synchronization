f624b0d6a9258ab85bfdea633428570de066ae8d
Alexis Rico <sferadev@gmail.com>
2019-03-12 23:44:22 +0100

    Add Sync FAB button

--- src/components/sync-configurator/BaseSyncConfigurator.jsx
+++ src/components/sync-configurator/BaseSyncConfigurator.jsx
@@ -3,11 +3,25 @@ import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 import i18n from "@dhis2/d2-i18n";
 import { ObjectsTable } from "d2-ui-components";
+import Fab from "@material-ui/core/Fab";
+import { withStyles } from "@material-ui/core/styles";
+import SyncIcon from '@material-ui/icons/Sync';
 import PageHeader from "../shared/PageHeader";
+
+const styles = theme => ({
+    fab: {
+        margin: theme.spacing.unit,
+        position: "fixed",
+        bottom: theme.spacing.unit * 5,
+        right: theme.spacing.unit * 5,
+    },
+    tableContainer: { marginTop: -10 },
+});
 
 class BaseSyncConfigurator extends React.Component {
     state = {
         tableKey: Math.random(),
+        selection: []
     };
 
     static propTypes = {
@@ -30,8 +44,16 @@ class BaseSyncConfigurator extends React.Component {
         this.props.history.push("/");
     };
 
+    selectionChange = (selection) => {
+        this.setState({ selection });
+    };
+
+    startSynchronization = () => {
+        // TODO: Render new dialog to show summary and select destination instances
+    };
+
     render() {
-        const { d2, model, title } = this.props;
+        const { d2, model, title, classes } = this.props;
 
         // Wrapper method to preserve static context
         const list = (...params) => model.listMethod(...params);
@@ -39,7 +61,7 @@ class BaseSyncConfigurator extends React.Component {
         return (
             <React.Fragment>
                 <PageHeader onBackClick={this.backHome} title={title} />
-                <div style={{ marginTop: -10 }}>
+                <div className={classes.tableContainer}>
                     <ObjectsTable
                         key={this.state.tableKey}
                         d2={d2}
@@ -50,11 +72,22 @@ class BaseSyncConfigurator extends React.Component {
                         initialSorting={model.getInitialSorting()}
                         actions={this.actions}
                         list={list}
+                        onSelectionChange={this.selectionChange}
                     />
+                    <Fab
+                        color="primary"
+                        aria-label="Add"
+                        className={classes.fab}
+                        size="large"
+                        onClick={this.startSynchronization}
+                        data-test="list-action-bar"
+                    >
+                        <SyncIcon />
+                    </Fab>
                 </div>
             </React.Fragment>
         );
     }
 }
 
-export default withRouter(BaseSyncConfigurator);
+export default withRouter(withStyles(styles)(BaseSyncConfigurator));
