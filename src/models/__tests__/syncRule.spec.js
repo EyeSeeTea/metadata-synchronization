6bae09600b072830f5a267b4a1e9340618187d3d
Jorge Sánchez <xurxosf@gmail.com>
2020-01-21 15:49:56 +0100

    Add tests to verify useDefaultIncludeExclude changes

--- src/models/__tests__/syncRule.spec.js
+++ src/models/__tests__/syncRule.spec.js
@@ -1,5 +1,7 @@
 import Instance from "../instance";
 import SyncRule from "../syncRule";
+import { DataElementModel, IndicatorModel } from "../d2Model";
+import { type } from "os";
 
 function givenASyncRuleWithMetadataIncludeExcludeRules() {
     return new SyncRule({
@@ -25,9 +27,76 @@ function givenASyncRuleWithMetadataIncludeExcludeRules() {
     });
 }
 
+function givenASyncRuleWithoutMetadataIncludeExcludeRules() {
+    return new SyncRule({
+        id: "",
+        name: "",
+        description: "",
+        builder: {
+            metadataIncludeExcludeRules: undefined,
+            useDefaultIncludeExclude: true,
+            targetInstances: [],
+            metadataIds: ["id1", "id2"],
+        },
+        enabled: false,
+    });
+}
+
 describe("SyncRule", () => {
+    describe("change useDefaultIncludeExclude", () => {
+        it("should reset to empty existed exclude include rules if set to true", () => {
+            const syncRule = givenASyncRuleWithMetadataIncludeExcludeRules();
+
+            const editedSyncRule = syncRule.markToUseDefaultIncludeExclude();
+
+            expect(editedSyncRule.useDefaultIncludeExclude).toEqual(true);
+            expect(editedSyncRule.metadataExcludeIncludeRules).toEqual(undefined);
+        });
+        it("should assign default exclude include rules in models if set to false", () => {
+            const syncRule = givenASyncRuleWithoutMetadataIncludeExcludeRules();
+
+            const editedSyncRule = syncRule.markToNotUseDefaultIncludeExclude([
+                DataElementModel,
+                IndicatorModel,
+            ]);
+
+            expect(editedSyncRule.useDefaultIncludeExclude).toEqual(false);
+            expect(editedSyncRule.metadataExcludeIncludeRules).toEqual({
+                indicators: {
+                    includeRules: [
+                        "attributes",
+                        "legendSets",
+                        "indicatorType",
+                        "indicatorGroups",
+                        "indicatorGroups.attributes",
+                        "indicatorGroups.indicatorGroupSet",
+                    ],
+                    excludeRules: ["dataSets", "programs"],
+                },
+                dataElements: {
+                    includeRules: [
+                        "attributes",
+                        "dataSets",
+                        "legendSets",
+                        "optionSets",
+                        "optionSets.options",
+                        "categoryCombos",
+                        "categoryCombos.attributes",
+                        "categoryCombos.categoryOptionCombos",
+                        "categoryCombos.categoryOptionCombos.categoryOptions",
+                        "categoryCombos.categories",
+                        "dataElementGroups",
+                        "dataElementGroups.attributes",
+                        "dataElementGroups.dataElementGroupSets",
+                        "dataElementGroups.dataElementGroupSets.attributes",
+                    ],
+                    excludeRules: [],
+                },
+            });
+        });
+    });
     describe("change metadataId", () => {
-        it("should reset exclude include rules if really has changed", () => {
+        it("should reset to empty exclude include rules if really has changed", () => {
             const syncRule = givenASyncRuleWithMetadataIncludeExcludeRules();
 
             const editedSyncRule = syncRule.updateMetadataIds(["newId1", "newId2"]);
