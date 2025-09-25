b0c7d4e8a35690b59d8a767ea6fe3f31b8ed91d7
xurxodev <xurxosf@gmail.com>
2025-09-25 09:43:24 +0200

    Set authType default

--- src/domain/instance/entities/Instance.ts
+++ src/domain/instance/entities/Instance.ts
@@ -174,6 +174,7 @@ export class Instance extends ShareableEntity<InstanceData> {
             },
             userAccesses: [],
             userGroupAccesses: [],
+            authType: "http-basic",
             ...data,
         });
     }
