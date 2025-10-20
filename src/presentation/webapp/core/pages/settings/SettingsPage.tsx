6b490e8efb10b7d009cd4f4e503151228b3e1578
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-10-20 13:20:43 +0000

    add using settings in settings page

--- src/presentation/webapp/core/pages/settings/SettingsPage.tsx
+++ src/presentation/webapp/core/pages/settings/SettingsPage.tsx
@@ -7,6 +7,8 @@ import i18n from "../../../../../utils/i18n";
 import PageHeader from "../../../../react/core/components/page-header/PageHeader";
 import { StorageSettingDropdown } from "./storage/StorageSettingDropdown";
 import { useSettings } from "./useSettings";
+import { useUserSettings } from "./useUserSettings";
+import { InclusionFields } from "../../../../react/core/components/sync-wizard/metadata/InclusionFields";
 
 export const SettingsPage: React.FC = () => {
     const history = useHistory();
@@ -27,6 +29,13 @@ export const SettingsPage: React.FC = () => {
         setStorageType,
         info,
     } = useSettings();
+
+    const { userSettings, updateUserSettingsInclusionConfig, saveUserSettings, inclusionOptions } = useUserSettings();
+
+    const handleSave = useCallback(async () => {
+        await saveUserSettings();
+        onSave();
+    }, [onSave, saveUserSettings]);
 
     const backHome = useCallback(() => history.push("/dashboard"), [history]);
 
@@ -80,12 +89,39 @@ export const SettingsPage: React.FC = () => {
                     onChange={onChangeRetentionDays}
                 />
 
+                {userSettings && (
+                    <FormGroup className={classes.content}>
+                        <h4 className={classes.subsection}>{i18n.t("Metadata Sync User Settings")}</h4>
+
+                        <InclusionFields
+                            sharingSettings={{
+                                value: userSettings.inclusionConfig.sharing,
+                                options: inclusionOptions,
+                                onValueChange: value => updateUserSettingsInclusionConfig("sharing", value),
+                                label: i18n.t("Default owner and sharing settings inclusion method"),
+                            }}
+                            users={{
+                                value: userSettings.inclusionConfig.users,
+                                options: inclusionOptions,
+                                onValueChange: value => updateUserSettingsInclusionConfig("users", value),
+                                label: i18n.t("Default users inclusion method"),
+                            }}
+                            orgUnits={{
+                                value: userSettings.inclusionConfig.organisationUnit,
+                                options: inclusionOptions,
+                                onValueChange: value => updateUserSettingsInclusionConfig("organisationUnit", value),
+                                label: i18n.t("Default organisation units inclusion method"),
+                            }}
+                        />
+                    </FormGroup>
+                )}
+
                 <ButtonsContainer>
                     <Button key={"cancel"} autoFocus onClick={onCancel}>
                         {i18n.t("Cancel")}
                     </Button>
 
-                    <Button key={"save"} color="primary" onClick={onSave}>
+                    <Button key={"save"} color="primary" onClick={handleSave}>
                         {i18n.t("Save")}
                     </Button>
                 </ButtonsContainer>
@@ -98,6 +134,7 @@ export const SettingsPage: React.FC = () => {
 const useStyles = makeStyles({
     content: { margin: "1rem", marginBottom: 35, marginLeft: 0 },
     title: { marginTop: 0 },
+    subsection: { marginTop: "1rem" },
     container: { margin: "1rem", padding: "1rem" },
 });
 
---
+++ src/presentation/webapp/core/pages/settings/useUserSettings.ts
@@ -0,0 +1,57 @@
+import React, { useCallback, useEffect } from "react";
+
+import { useAppContext } from "../../../../react/core/contexts/AppContext";
+import { UserSettings } from "../../../../../domain/user-settings/UserSettings";
+import { useSnackbar } from "@eyeseetea/d2-ui-components";
+import {
+    includeObjectsAndReferencesMap,
+    includeObjectsAndReferencesOptions,
+} from "../../../../react/core/components/sync-wizard/metadata/useMetadataIncludeExcludeStep";
+
+export function useUserSettings() {
+    const { newCompositionRoot } = useAppContext();
+    const snackbar = useSnackbar();
+
+    const [userSettings, setUserSettings] = React.useState<UserSettings>();
+
+    const updateUserSettingsInclusionConfig = useCallback(
+        <K extends keyof UserSettings["inclusionConfig"]>(key: K, value: UserSettings["inclusionConfig"][K]) => {
+            if (!userSettings) return;
+            else {
+                setUserSettings(userSettings => userSettings?.updateInclusionConfig(key, value));
+            }
+        },
+        [userSettings]
+    );
+
+    const saveUserSettings = useCallback(() => {
+        if (!userSettings) return;
+        else {
+            return newCompositionRoot.userSettings.save
+                .execute(userSettings)
+                .toPromise()
+                .then(() => snackbar.info("User settings saved successfully!"))
+                .catch(error => {
+                    console.error(`error saving user settings:  ${error}`);
+                    snackbar.error("Error saving user settings. Please try again later.");
+                });
+        }
+    }, [newCompositionRoot, userSettings, snackbar]);
+
+    useEffect(() => {
+        return newCompositionRoot.userSettings.get.execute().run(
+            userSettings => setUserSettings(userSettings),
+            error => {
+                console.error(`error fetching user settings:  ${error}`);
+                snackbar.error("Error fetching user settings. Please try again later.");
+            }
+        );
+    }, [newCompositionRoot, snackbar]);
+
+    return {
+        userSettings,
+        updateUserSettingsInclusionConfig,
+        saveUserSettings,
+        inclusionOptions: includeObjectsAndReferencesOptions,
+    };
+}
