71b34deba106e132f525ac3d904080d65a1b74ef
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-10-20 13:19:30 +0000

    add domain and data UserSettings

---
+++ src/domain/user-settings/UserSettings.ts
@@ -0,0 +1,31 @@
+import { Struct } from "../common/entities/Struct";
+
+export const DEFAULT_INCLUSION_MODE = "includeObjectsAndReferences";
+export const DEFAULT_USER_SETTINGS: UserSettingsProps = {
+    inclusionConfig: {
+        sharing: DEFAULT_INCLUSION_MODE,
+        users: DEFAULT_INCLUSION_MODE,
+        organisationUnit: DEFAULT_INCLUSION_MODE,
+    },
+};
+
+const inclusionModes = ["includeObjectsAndReferences", "includeOnlyReferences", "removeObjectsAndReferences"] as const;
+
+type InclusionMode = typeof inclusionModes[number];
+
+export type UserSettingsProps = {
+    inclusionConfig: {
+        sharing: InclusionMode;
+        users: InclusionMode;
+        organisationUnit: InclusionMode;
+    };
+};
+
+export class UserSettings extends Struct<UserSettingsProps>() {
+    updateInclusionConfig<K extends keyof UserSettings["inclusionConfig"]>(
+        key: K,
+        value: UserSettings["inclusionConfig"][K]
+    ): UserSettings {
+        return this._update({ inclusionConfig: { ...this.inclusionConfig, [key]: value } });
+    }
+}
