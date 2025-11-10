938fda75aca4ba9d220d934e64ac50370fb37732
xurxodev <xurxosf@gmail.com>
2025-11-10 09:18:08 +0100

    Format the code

--- src/data/aggregated/getAggregatedDataExchangeJobConfigurations.ts
+++ src/data/aggregated/getAggregatedDataExchangeJobConfigurations.ts
@@ -18,7 +18,3 @@ export async function getAggregatedDataExchangeJobConfigurations(
 
     return response.jobConfigurations;
 }
-
-
-
-
--- src/data/rules/models/SyncRulePersistedData.ts
+++ src/data/rules/models/SyncRulePersistedData.ts
@@ -1,8 +1,5 @@
 import { SynchronizationRuleData } from "../../../domain/rules/entities/SynchronizationRule";
 
-export type SyncRulePersistedData = Omit<
-    SynchronizationRuleData,
-    "aggregatedDataExchanges" 
-> & {
+export type SyncRulePersistedData = Omit<SynchronizationRuleData, "aggregatedDataExchanges"> & {
     aggregatedDataExchanges?: { id: string }[];
 };
